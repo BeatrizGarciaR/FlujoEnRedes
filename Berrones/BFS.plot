fa10db3db28a9ffb747171b081b4799d3ebba9b3
Beatriz Alejandra García Ramos <BeatrizGarcia@nestor-cids-imac.local>
2018-04-12 18:44:01 -0500

    Mejorado

--- Berrones/BFS.plot
+++ Berrones/BFS.plot
@@ -5,24 +5,44 @@ set yrange [-.1:1.1]
 set pointsize 2
 set size square
 set key off
-set arrow 1 from 0.392164, 0.162659 to 0.837513, 0.115946 nohead filled lw 1
-set arrow 2 from 0.837513, 0.115946 to 0.392164, 0.162659 nohead filled lw 1
-set arrow 3 from 0.250041, 0.905135 to 0.227796, 0.817467 nohead filled lw 1
-set arrow 4 from 0.227796, 0.817467 to 0.250041, 0.905135 nohead filled lw 1
-set arrow 5 from 0.945925, 0.115269 to 0.837513, 0.115946 nohead filled lw 1
-set arrow 6 from 0.837513, 0.115946 to 0.945925, 0.115269 nohead filled lw 1
-set arrow 7 from 0.608930, 0.646338 to 0.539537, 0.692599 nohead filled lw 1
-set arrow 8 from 0.539537, 0.692599 to 0.608930, 0.646338 nohead filled lw 1
-set arrow 9 from 0.812907, 0.693171 to 0.608930, 0.646338 nohead filled lw 1
-set arrow 10 from 0.608930, 0.646338 to 0.812907, 0.693171 nohead filled lw 1
-set arrow 11 from 0.113267, 0.541128 to 0.214249, 0.632093 nohead filled lw 1
-set arrow 12 from 0.214249, 0.632093 to 0.113267, 0.541128 nohead filled lw 1
-set arrow 13 from 0.227796, 0.817467 to 0.837513, 0.115946 nohead filled lw 1
-set arrow 14 from 0.837513, 0.115946 to 0.227796, 0.817467 nohead filled lw 1
-set arrow 15 from 0.837513, 0.115946 to 0.214249, 0.632093 nohead filled lw 1
-set arrow 16 from 0.214249, 0.632093 to 0.837513, 0.115946 nohead filled lw 1
-set arrow 17 from 0.812907, 0.693171 to 0.837513, 0.115946 nohead filled lw 1
-set arrow 18 from 0.837513, 0.115946 to 0.812907, 0.693171 nohead filled lw 1
+set arrow 1 from 0.455797, 0.239609 to 0.574082, 0.122226 nohead filled lw 1
+set arrow 2 from 0.574082, 0.122226 to 0.455797, 0.239609 nohead filled lw 1
+set arrow 3 from 0.234968, 0.668850 to 0.312059, 0.562906 nohead filled lw 1
+set arrow 4 from 0.312059, 0.562906 to 0.234968, 0.668850 nohead filled lw 1
+set arrow 5 from 0.438581, 0.706166 to 0.362311, 0.842488 nohead filled lw 1
+set arrow 6 from 0.362311, 0.842488 to 0.438581, 0.706166 nohead filled lw 1
+set arrow 7 from 0.845939, 0.808277 to 0.624324, 0.483971 nohead filled lw 1
+set arrow 8 from 0.624324, 0.483971 to 0.845939, 0.808277 nohead filled lw 1
+set arrow 9 from 0.047883, 0.753621 to 0.026377, 0.626429 nohead filled lw 1
+set arrow 10 from 0.026377, 0.626429 to 0.047883, 0.753621 nohead filled lw 1
+set arrow 11 from 0.293007, 0.475632 to 0.312059, 0.562906 nohead filled lw 1
+set arrow 12 from 0.312059, 0.562906 to 0.293007, 0.475632 nohead filled lw 1
+set arrow 13 from 0.563588, 0.043753 to 0.574082, 0.122226 nohead filled lw 1
+set arrow 14 from 0.574082, 0.122226 to 0.563588, 0.043753 nohead filled lw 1
+set arrow 15 from 0.026377, 0.626429 to 0.069755, 0.535413 nohead filled lw 1
+set arrow 16 from 0.069755, 0.535413 to 0.026377, 0.626429 nohead filled lw 1
+set arrow 17 from 0.624324, 0.483971 to 0.438581, 0.706166 nohead filled lw 1
+set arrow 18 from 0.438581, 0.706166 to 0.624324, 0.483971 nohead filled lw 1
+set arrow 19 from 0.139835, 0.057722 to 0.455797, 0.239609 nohead filled lw 1
+set arrow 20 from 0.455797, 0.239609 to 0.139835, 0.057722 nohead filled lw 1
+set arrow 21 from 0.770113, 0.144699 to 0.574082, 0.122226 nohead filled lw 1
+set arrow 22 from 0.574082, 0.122226 to 0.770113, 0.144699 nohead filled lw 1
+set arrow 23 from 0.574082, 0.122226 to 0.293007, 0.475632 nohead filled lw 1
+set arrow 24 from 0.293007, 0.475632 to 0.574082, 0.122226 nohead filled lw 1
+set arrow 25 from 0.293007, 0.475632 to 0.438581, 0.706166 nohead filled lw 1
+set arrow 26 from 0.438581, 0.706166 to 0.293007, 0.475632 nohead filled lw 1
+set arrow 27 from 0.362311, 0.842488 to 0.455797, 0.239609 nohead filled lw 1
+set arrow 28 from 0.455797, 0.239609 to 0.362311, 0.842488 nohead filled lw 1
+set arrow 29 from 0.455797, 0.239609 to 0.293007, 0.475632 nohead filled lw 1
+set arrow 30 from 0.293007, 0.475632 to 0.455797, 0.239609 nohead filled lw 1
+set arrow 31 from 0.047883, 0.753621 to 0.563588, 0.043753 nohead filled lw 1
+set arrow 32 from 0.563588, 0.043753 to 0.047883, 0.753621 nohead filled lw 1
+set arrow 33 from 0.026377, 0.626429 to 0.438581, 0.706166 nohead filled lw 1
+set arrow 34 from 0.438581, 0.706166 to 0.026377, 0.626429 nohead filled lw 1
+set arrow 35 from 0.438581, 0.706166 to 0.047883, 0.753621 nohead filled lw 1
+set arrow 36 from 0.047883, 0.753621 to 0.438581, 0.706166 nohead filled lw 1
+set arrow 37 from 0.312059, 0.562906 to 0.574082, 0.122226 nohead filled lw 1
+set arrow 38 from 0.574082, 0.122226 to 0.312059, 0.562906 nohead filled lw 1
 plot 'BFS.dat' using 1:2:3 with points pt 7 lc var
 show arrow
 quit()
--- Berrones/BFS.py
+++ Berrones/BFS.py
@@ -65,12 +65,8 @@ class Grafo:
                 for k in range (len(self.dismin)):
                     lis.append(items[k][1])
                 u = items[lis.index(min(self.dismin.values()))][0][1]
-                if pesos is 0:
-                  self.aristas[((x1,y1),u)] = pesos
-                  self.vecinos[(x1,y1)].add(u)
-                else:
-                  self.aristas[((x1,y1),u)] = randint(1,10)
-                  self.vecinos[(x1,y1)].add(u)
+                self.aristas[((x1,y1),u)] = 0
+                self.vecinos[(x1,y1)].add(u)
                 del(self.dis[((x1,y1),u)])
 
 
@@ -94,18 +90,12 @@ class Grafo:
                 for k in range (len(self.dismin)):
                     lis.append(items[k][1])
                 u = items[lis.index(min(self.dismin.values()))][0][1]
-                if pesos is 0:
-                  self.aristas[((x1,y1),u)] = self.aristas[(u,(x1,y1))] = pesos
-                  self.vecinos[(x1,y1)].add(u)
-                  self.vecinos[u].add((x1,y1))
-                else:
-                  self.aristas[((x1,y1),u)] = self.aristas[(u,(x1,y1))] = randint(1,10)
-                  self.vecinos[(x1,y1)].add(u)
-                  self.vecinos[u].add((x1,y1))
+                self.aristas[((x1,y1),u)] = self.aristas[(u,(x1,y1))] = 0
+                self.vecinos[(x1,y1)].add(u)
+                self.vecinos[u].add((x1,y1))
                 del(self.dis[((x1,y1),u)])
                 del(self.dis[(u,(x1,y1))])
                     
-        
 
     def conectaAleatorioDir(self):
         import random
@@ -115,19 +105,12 @@ class Grafo:
             u = random.sample(self.nodos,1)[0]
             if v == u:
                 u = random.sample(self.nodos,1)[0]
-                if pesos is 0:
-                    self.aristas[(v,u)] = pesos
-                    self.vecinos[v].add(u)
-                else:
-                    self.aristas[(v,u)] = randint(1,10)
-                    self.vecinos[v].add(u)
+                self.aristas[(v,u)] = 0
+                self.vecinos[v].add(u)
             else:
-                if pesos is 0:
-                    self.aristas[(v,u)] = pesos
-                    self.vecinos[v].add(u)
-                else:
-                    self.aristas[(v,u)] = randint(1,10)
-                    self.vecinos[v].add(u)
+                self.aristas[(v,u)] = 0
+                self.vecinos[v].add(u)
+                
 
     def conectaAleatorio(self):
         import random
@@ -137,23 +120,14 @@ class Grafo:
             u = random.sample(self.nodos,1)[0]
             if v == u:
                 u = random.sample(self.nodos,1)[0]
-                if pesos is 0:
-                    self.aristas[(v,u)] = self.aristas[(u,v)] = pesos
-                    self.vecinos[v].add(u)
-                    self.vecinos[u].add(v)
-                else:
-                    self.aristas[(v,u)] = self.aristas[(u,v)] = randint(1,10)
-                    self.vecinos[v].add(u)
-                    self.vecinos[u].add(v)
+                self.aristas[(v,u)] = self.aristas[(u,v)] = 0
+                self.vecinos[v].add(u)
+                self.vecinos[u].add(v)
             else:
-                if pesos is 0:
-                    self.aristas[(v,u)] = self.aristas[(u,v)] = pesos
-                    self.vecinos[v].add(u)
-                    self.vecinos[u].add(v)
-                else:
-                    self.aristas[(v,u)] = self.aristas[(u,v)] = randint(1,10)
-                    self.vecinos[v].add(u)
-                    self.vecinos[u].add(v)
+                self.aristas[(v,u)] = self.aristas[(u,v)] = 0
+                self.vecinos[v].add(u)
+                self.vecinos[u].add(v)
+                
 
     def Breath_First_Search(self):
         import random
@@ -176,7 +150,7 @@ class Grafo:
         return explorado
 
 
-    def graficar(self, di, pesos):
+    def graficar(self, di):
         with open("BFS.plot","w") as archivo:
              print("set term png", file=archivo)
              print("set output 'BFS.png'", file=archivo)
@@ -186,44 +160,23 @@ class Grafo:
              print("set size square", file=archivo)
              print("set key off", file=archivo)
              num=0
-             set1 = [1,2,3]
-             set2 = [4,5,6]
-             set3 = [7,8,9,10]
              for key in self.aristas:
                  x1 = key[0][0] 
                  y1 = key[0][1] 
                  x2 = key[1][0] 
                  y2 = key[1][1]
                  if di is 1:
-                    if x1 < x2:
-                     x2 = x2 - .007
+                     if x1 < x2:
+                         x2 = x2 - .007
                      if x2 < x1:
                          x2 = x2 + .007
                      if y1 < y2:
                          y2 = y2 - .007
                      if y2 < y1:
                          y2 = y2 + .007
-                    if pesos is 0:
-                        print("set arrow {:d} from {:f},{:f} to {:f}, {:f} head filled size screen 0.02,15,45 lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
-                    else:
-                        p = self.aristas[key]
-                        if p in set1:
-                            print("set arrow {:d} from {:f},{:f} to {:f}, {:f} head filled size screen 0.02,15,45 lw 1.5 lc 1".format(num+1,x1,y1,x2,y2),file=archivo)
-                        if p in set2:
-                            print("set arrow {:d} from {:f},{:f} to {:f}, {:f} head filled size screen 0.02,15,45 lw 1.5 lc 2".format(num+1,x1,y1,x2,y2),file=archivo)
-                        if p in set3:
-                            print("set arrow {:d} from {:f},{:f} to {:f}, {:f} head filled size screen 0.02,15,45 lw 1.5 lc 6".format(num+1,x1,y1,x2,y2),file=archivo)
+                     print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} head filled size screen 0.02,15,45 lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
                  else:
-                     if pesos is 0:
-                        print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
-                     else:
-                        p = self.aristas[key]
-                        if p in set1:
-                            print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1.5 lc 1".format(num+1,x1,y1,x2,y2),file=archivo)
-                        if p in set2:
-                            print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1.5 lc 2".format(num+1,x1,y1,x2,y2),file=archivo)
-                        if p in set3:
-                            print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1.5 lc 6".format(num+1,x1,y1,x2,y2),file=archivo)        
+                     print("set arrow {:d} from {:f}, {:f} to {:f}, {:f} nohead filled lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
                  num+=1
              print("plot 'BFS.dat' using 1:2:3 with points pt 7 lc var", file=archivo)
              print("show arrow", file=archivo)
@@ -232,10 +185,9 @@ class Grafo:
 
 
 di = 0      #Si di=0 el grafo va a ser sin direccion, si es 1 es dirigido
-pesos = 0   #Si pesos=0 el grafo no tendra ponderacion, si es 1 si lo tendra
 
 if di is 0:
-    i = 10 #Cantidad de nodos que va a tener el grafo
+    i = 15 #Cantidad de nodos que va a tener el grafo
     G = Grafo()
     G.agrega(i)
     G.guarda()
@@ -243,11 +195,11 @@ if di is 0:
     G.conecta()
     G.conectaAleatorio()
     print(G.Breath_First_Search())
-    G.graficar(di,pesos)
+    G.graficar(di)
 
 
 if di is 1:
-    i = 10 #Cantidad de nodos que va a tener el grafo
+    i = 15 #Cantidad de nodos que va a tener el grafo
     G = Grafo()
     G.agrega(i)
     G.guarda()
@@ -255,6 +207,6 @@ if di is 1:
     G.conectaDir()
     G.conectaAleatorioDir()
     print(G.Breath_First_Search())
-    G.graficar(di,pesos)
+    G.graficar(di)
 
                 
