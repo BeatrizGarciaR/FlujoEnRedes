89d2eb7cc1dc4a34bc3af552925d602f2b19dca8
Beatriz Alejandra García Ramos <BeatrizGarcia@nestor-cids-imac.local>
2018-04-16 20:07:31 -0500

    Algoritmo incluído

--- Tarea4/Tarea4Base.py
+++ Tarea4/Tarea4Base.py
@@ -1,5 +1,5 @@
 from random import random
-from math import sqrt, ceil, sin, cos, pi
+from math import sqrt, ceil, sin, cos, pi, floor
 from random import randint, uniform, random
 import time, random
 
@@ -13,7 +13,7 @@ class Grafo:
         
 
     def agrega(self, i):
-        with open("NodosWarshall.dat", "at") as crear:
+        with open("NodosWarshall.dat", "w") as crear:
             centro = (0.5, 0.5)
             radio = 0.4
             angulo = (360/i)*(pi/180)
@@ -31,7 +31,7 @@ class Grafo:
             for j in range(0,i):
                 a = self.nodos[j]
                 b = self.nodos[j-r]
-                self.aristas[(a,b)] = self.aristas[(b,a)] = 2
+                self.aristas[(a,b)] = self.aristas[(b,a)] = r
                 self.vecinos[a].add(b)
                 self.vecinos[b].add(a)
 
@@ -39,17 +39,46 @@ class Grafo:
         for (x1,y1) in self.nodos:
             for (x2,y2) in self.nodos:
                 rand = random.uniform(0,1)
-                print(rand)
                 if rand < p and ((x1,y1),(x2,y2)) not in self.aristas and ((x2,y2),(x1,y1)) not in self.aristas:
-                    self.aristas[((x1,y1),(x2,y2))] = self.aristas[((x2,y2),(x1,y1))] = 2
+                    self.kmax = floor(i/2)
+                    u = self.nodos.index((x1,y1))
+                    v = self.nodos.index((x2,y2))
+                    dis = abs(u-v)
+                    if dis > self.kmax:
+                        dis2 = i - dis
+                        self.aristas[((x1,y1),(x2,y2))] = self.aristas[((x2,y2),(x1,y1))] = dis2
+                    else:
+                        self.aristas[((x1,y1),(x2,y2))] = self.aristas[((x2,y2),(x1,y1))] = dis                       
                     self.vecinos[(x1,y1)].add((x2,y2))
                     self.vecinos[(x2,y2)].add((x1,y1))
+                        
+
+    def floyd_warshall(self):
+        self.d = {}
+        for (x,y) in self.nodos:
+            self.d[((x,y),(x,y))] = 0 # distancia reflexiva es cero
+            for u in self.vecinos[(x,y)]: # para vecinos, la distancia es el peso
+                self.d[((x,y),u)] = self.aristas[((x,y),u)]
+        for intermedio in self.nodos:
+            for desde in self.nodos:
+                for hasta in self.nodos:
+                    di = None
+                    if (desde, intermedio) in self.d:
+                        di = self.d[(desde, intermedio)]
+                    ih = None
+                    if (intermedio, hasta) in self.d:
+                        ih = self.d[(intermedio, hasta)]
+                    if di is not None and ih is not None:
+                        c = di + ih # largo del camino via "i"
+                        if (desde, hasta) not in self.d or c < self.d[(desde, hasta)]:
+                            self.d[(desde, hasta)] = c # mejora al camino actual
+        return self.d
                     
 
     def graficar(self):
         with open("Tarea4.plot","w") as archivo:
-             print("set term pdf", file=archivo)
-             print("set output 'Tarea4.pdf'", file=archivo)
+             print("set term png", file=archivo)
+             print("set output 'Tarea4.png'", file=archivo)
              print("set xrange [-.1:1.1]", file=archivo)
              print("set yrange [-.1:1.1]", file=archivo)
              print("set pointsize .7", file=archivo)
@@ -61,7 +90,11 @@ class Grafo:
                  y1 = key[0][1] 
                  x2 = key[1][0] 
                  y2 = key[1][1]
-                 print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
+                 p = self.aristas[key]
+                 if p > k:
+                     print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1 lc 1".format(num+1,x1,y1,x2,y2),file=archivo)
+                 else:
+                     print("set arrow {:d} from {:f},{:f} to {:f}, {:f} nohead lw 1".format(num+1,x1,y1,x2,y2),file=archivo)
                  num+=1
              print("plot 'NodosWarshall.dat' using 1:2 with points pt 7 lc 6", file=archivo)
              print("show arrow", file=archivo)
@@ -69,11 +102,12 @@ class Grafo:
 
 
 
-i = 10
+i = 11
 k = ceil(i/4)
 p = 0.1
 G = Grafo()
 G.agrega(i)
 G.conecta(k)
 G.conectaAleatorio(p)
+G.floyd_warshall()
 G.graficar()
